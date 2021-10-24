eb8a4e619929e35f836edafe70ebbf08958321d5
Guillaume Quittet <guillaume.quittet@gmail.com>
2021-10-24 18:32:47 +0200

    test(timeframe): test generate_timeframe function

--- src/timeframe.rs
+++ src/timeframe.rs
@@ -1,6 +1,6 @@
 use chrono::NaiveDate;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Timeframe {
     pub start: i64,
     pub end: i64,
@@ -13,4 +13,16 @@ pub fn generate_timeframe(year: i32) -> Timeframe {
         .timestamp();
 
     Timeframe { start, end }
+}
+
+#[test]
+fn it_generate_correct_timeframe_with_positive_date() {
+    let expected = Timeframe {
+        start: 1609459200, // 2021-01-01 00:00:00 UTC
+        end: 1640995200,   // 2022-01-01 00:00:00 UTC
+    };
+
+    let result = generate_timeframe(2021);
+
+    assert_eq!(expected, result)
 }
