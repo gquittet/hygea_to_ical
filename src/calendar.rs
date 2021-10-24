8e6d405a48a7907c6ec152988472ae27a217bf6a
Guillaume Quittet <guillaume.quittet@gmail.com>
2021-10-24 18:33:29 +0200

    fix(calendar): filter entries to keep only requested ones
    The Hygea API has a bug that make it returns everything even if we give
    it the start and end timestamps for the time frame.
    
    So we have to filter the returned data to keep only those we want.

--- src/calendar.rs
+++ src/calendar.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use crate::{config::Config, timeframe::Timeframe};
 
-use chrono::DateTime;
+use chrono::{DateTime, NaiveDateTime};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -41,8 +41,22 @@ impl CalendarHTTPClient {
             ])
             .send()
             .await?
-            .json()
-            .await?;
+            .json::<Vec<CalendarEntry>>()
+            .await?
+            .into_iter()
+            .filter(|entry| {
+                // Keep only the entry that are in the timeframe.
+                // This is because of the API that return all the data even if the start and end
+                // query params are specified.
+                let date_parsed =
+                    NaiveDateTime::parse_from_str(&entry.start, "%Y-%m-%dT%H:%M:%S%z");
+                let start_timestamp = match date_parsed {
+                    Ok(date) => date.timestamp(),
+                    Err(error) => panic!("Unable to parse date from API: {:?}", error),
+                };
+                return start_timestamp >= timeframe.start && start_timestamp <= timeframe.end;
+            }) // Format: 2019-01-03T00:00:00+01:00
+            .collect();
 
         Ok(data)
     }
