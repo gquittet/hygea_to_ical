aaedc98661db3b1509992c9ec962e4374c8bec37
Guillaume Quittet <guillaume.quittet@gmail.com>
2021-10-28 02:13:54 +0200

    feat(config): use clap to handle cli args

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,12 @@
-#[derive(Debug, Clone, Copy)]
+use clap::{crate_authors, crate_description, crate_version, Parser};
+
+#[derive(Debug, Parser, Clone)]
+#[clap(
+    author = crate_authors!(),
+    about = crate_description!(),
+    version = crate_version!(),
+)]
 pub struct Config {
     pub postal_code: u16,
-}
-
-pub fn parse_config(args: &[String]) -> Config {
-    let postal_code = u16::from_str_radix(&args[1], 10).unwrap();
-
-    Config { postal_code }
+    pub street: Option<String>,
 }
