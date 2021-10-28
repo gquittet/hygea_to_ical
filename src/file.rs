d153622f35bec298dae052e0f5771050991274de
Guillaume Quittet <guillaume.quittet@gmail.com>
2021-10-28 02:14:45 +0200

    feat(ical): handle url with street index like Mons

--- src/file.rs
+++ src/file.rs
@@ -3,12 +3,11 @@ use tokio::fs::File;
 use tokio::io::{AsyncWriteExt, Error};
 use uuid::Uuid;
 
-use crate::config::Config;
-
 pub async fn ical_to_file(
     name: &'static str,
     ical_entries: HashMap<String, &'static str>,
-    config: Config,
+    postal_code: u16,
+    street_index: Option<u16>,
 ) -> Result<(), Error> {
     let mut buffer = File::create(name).await?;
 
@@ -33,7 +32,7 @@ DTSTART;TZID=Europe/Brussels;VALUE=DATE:{date}
 SEQUENCE:0
 TRANSP:OPAQUE
 DTEND;TZID=Europe/Brussels;VALUE=DATE:{date}
-URL:https://www.hygea.be/votre-calendrier-de-collecte.html?cp={postal_code}&streetIndex=
+URL:https://www.hygea.be/votre-calendrier-de-collecte.html?cp={postal_code}&streetIndex={street_index}
 SUMMARY:Collecte des déchets
 DESCRIPTION:{description}
 X-MICROSOFT-CDO-ALLDAYEVENT:TRUE
@@ -42,7 +41,11 @@ END:VEVENT
                     uuid = Uuid::new_v4(),
                     date = date,
                     description = description.replace("\n", "\\n"),
-                    postal_code = config.postal_code,
+                    postal_code = postal_code,
+                    street_index = match street_index {
+                        Some(index) => index.to_string(),
+                        None => "".to_string(),
+                    }
                 )
                 .as_bytes(),
             )
